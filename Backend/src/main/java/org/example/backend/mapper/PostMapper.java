7414f17cc7411825e8378114a911dd2452e58405
Presnyakov Danila <danyapres2010@gmail.com>
2025-07-23 12:47:34 +0300

    рабочая версия. post,user мапер post,user dto

---
+++ Backend/src/main/java/org/example/backend/mapper/PostMapper.java
@@ -0,0 +1,25 @@
+package org.example.backend.mapper;
+
+
+import org.example.backend.dto.PostRequest;
+import org.example.backend.dto.PostResponse;
+import org.example.backend.model.Post;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PostMapper {
+    public Post toEntity(PostRequest dto) {
+        Post post = new Post();
+        post.setTitle(dto.getTitle());
+        post.setContent(dto.getContent());
+        return post;
+    }
+
+    public PostResponse toDto(Post post) {
+        PostResponse dto = new PostResponse();
+        dto.setId(post.getId());
+        dto.setTitle(post.getTitle());
+        dto.setContent(post.getContent());
+        return dto;
+    }
+}
