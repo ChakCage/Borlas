c2ece8b263c5280598d1aaffc98ac5c7ef87a831
ottovector <50682440+Otto-Vector@users.noreply.github.com>
2025-07-30 15:46:26 +0300

    разблокировали main

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import "./App.css"
-import {MainView} from "./views/main-view/main-view"
+import MainView from "./views/main-view/main-view"
 import {Footer} from "./views/footer/footer"
 
 function App() {
--- frontend/src/views/main-view/main-view.jsx
+++ frontend/src/views/main-view/main-view.jsx
@@ -1,30 +1,30 @@
-// import React from "react";
-// import "./main-view.css";
-// import { Link } from "react-router-dom";
-//
-// function Navbar() {
-//     return (
-//         <nav className="navbar">
-//             <ul className="navbar__list">
-//                 <li className="navbar__item">
-//                     <Link to="/" className="navbar__link">Домой</Link>
-//                 </li>
-//                 <li className="navbar__item">
-//                     <Link to="/posts" className="navbar__link">Посты</Link>
-//                 </li>
-//             </ul>
-//         </nav>
-//     );
-// }
-//
-// export default function MainView() {
-//     return (
-//         <div className="main-view__container">
-//             <Navbar />
-//             <div className="main-view__content">
-//                 {/* ... */}
-//             </div>
-//         </div>
-//     );
-// }
+import React from "react";
+import "./main-view.css";
+import { Link } from "react-router-dom";
+
+function Navbar() {
+    return (
+        <nav className="navbar">
+            <ul className="navbar__list">
+                <li className="navbar__item">
+                    <Link to="/" className="navbar__link">Домой</Link>
+                </li>
+                <li className="navbar__item">
+                    <Link to="/posts" className="navbar__link">Посты</Link>
+                </li>
+            </ul>
+        </nav>
+    );
+}
+
+export default function MainView() {
+    return (
+        <div className="main-view__container">
+            <Navbar />
+            <div className="main-view__content">
+                {}
+            </div>
+        </div>
+    );
+}
 
