6301e971cffde9308b07c8140a29e75fc7d0f5b6
MatveyGolshev <matveygolsev@gmail.com>
2025-08-19 09:55:14 +0300

    доработки

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -12,8 +12,8 @@ import './App.scss'
 import {Button} from "./components/button/Button"
 import {TextButton} from "./components/TextButton/TextButton"
 import {ButtonTheme, TextButtonTheme} from "./types/BtnThemeEnum"
-import ProfilePage from "./pages/MyProfilePage/MyProfilePage";
-import AuthModal from "./pages/AuthModal/AuthModal";
+// import ProfilePage from "./pages/MyProfilePage/MyProfilePage";
+// import AuthModal from "./pages/AuthModal/AuthModal";
 
 function App() {
     setAuthToken("testuser", "yourpassword")
@@ -35,24 +35,11 @@ function App() {
                     <Sidebar/>
                     <main className="App__main">
                         <Routes>
-                            <Route path="/" element={<AuthModal/>}/>
+                            <Route path="/" element={<PostsPage/>}/>
                             <Route path="/posts" element={<PostsPage/>}/>
                             <Route path="/create" element={<CreatePostPage/>}/>
                             <Route path="/my-posts" element={<MyPostsPage/>}/>
                             <Route path="/deleted" element={<DeletedPostsPage/>}/>
-                            <Route path="/my-profile" element={<ProfilePage/>}/>
-                            {/*<Route path="/testView" element={
-                                <div style={{padding: "100px", display: "flex", flexDirection: "column", gap: "15px"}}>
-                                    <Button text={"Button Primary"} onClick={onClickBtn}/>
-                                    <Button text={"Button Primary disabled"} onClick={onClickBtn} isDisabled={true}/>
-                                    <Button text={"Button Soft"} onClick={onClickBtn} theme={ButtonTheme.Soft}/>
-                                    <Button text={"Button Soft disabled"} onClick={onClickBtn} theme={ButtonTheme.Soft}
-                                            isDisabled={true}/>
-                                    <TextButton text={"Комментировать"} onClick={onClickTxtBtn} theme={TextButtonTheme.Primary}/>
-                                    <TextButton text={"Комментировать"} onClick={onClickTxtBtn} theme={TextButtonTheme.Primary} isDisabled={true}/>
-                                    <TextButton text={"Комментировать"} onClick={onClickTxtBtn} theme={TextButtonTheme.Soft}/>
-                                    <TextButton text={"Комментировать"} onClick={onClickTxtBtn} theme={TextButtonTheme.Soft} isDisabled={true}/>
-                                </div>}/>*/}
                         </Routes>
                     </main>
                 </div>
--- frontend/src/components/Sidebar/SideItems.tsx
+++ frontend/src/components/Sidebar/SideItems.tsx
@@ -1,16 +1,12 @@
 import DynamicFeedIcon from '@mui/icons-material/DynamicFeed';
 import AddIcon from '@mui/icons-material/Add';
 import PersonIcon from '@mui/icons-material/Person';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import DeleteIcon from '@mui/icons-material/Delete';
-import LockOpenIcon from '@mui/icons-material/LockOpen';
 
 export const items = [
-    { to: '/auth', icon: <LockOpenIcon />, label: 'Авторизация' },
     { to: '/', icon: <DynamicFeedIcon />, label: 'Все посты' },
     { to: '/create', icon: <AddIcon />, label: 'Создать пост' },
     { to: '/my-posts', icon: <PersonIcon />, label: 'Мои посты' },
-    { to: '/my-profile', icon: <AccountCircleIcon />, label: 'Мой профиль' },
     { to: '/deleted', icon: <DeleteIcon />, label: 'Удаленные посты' },
     // { to: '/testView', icon: null, label: 'Тестовый экран' },
 ];
